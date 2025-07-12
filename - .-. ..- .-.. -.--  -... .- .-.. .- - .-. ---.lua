3651ae1b082d17f215f10c1d82d36c5eb3c7bf35
normalcat <tntgav@gmail.com>
2025-07-12 13:05:57 -0700

    convert the left lily into the right lily

--- - .-. ..- .-.. -.--  -... .- .-.. .- - .-. ---.lua
+++ - .-. ..- .-.. -.--  -... .- .-.. .- - .-. ---.lua
@@ -17,6 +17,14 @@ local function rand_letters(str, amount)
     return s
 end
 
+local function rnd_joker_with_calc()
+    local pick = {}
+    while (not pick.calculate) do
+        pick = G.P_CENTER_POOLS.Joker[math.random(1,#G.P_CENTER_POOLS.Joker)]
+    end
+    return pick
+end
+
 local oldplaysound = play_sound
 function play_sound(sound_code, per, vol)
     -- return oldplaysound(sound_code, per, vol)
@@ -37,9 +45,9 @@ function generate_card_ui(_c,full_UI_table,specific_vars,card_type,badges,hide_d
         for j,k in ipairs(tab.main[i]) do
             if k.config.text then
                 -- k.config.text = k.config.text .. (tab.main[i][j-1] and tab.main[i][j-1].config.text or "") .. (tab.main[i][j+1] and tab.main[i][j+1].config.text or "")
-                k.config.text = rand_letters(k.config.text, #k.config.text)
+                -- k.config.text = rand_letters(k.config.text, #k.config.text)
                 k.config.colour = {math.random(), math.random(), math.random(), 1}
-                k.config.scale = math.random()*0.7
+                k.config.scale = math.random()
             end
         end
     end
@@ -68,4 +76,37 @@ function SMODS.calculate_individual_effect(effect, scored_card, key, amount, fro
     end
 
     return individualeffect(effect, scored_card, key, amount, from_edition)
+end
+
+local function mod_number(x)
+    if type(x) == "table" and (not x.tetrate) then
+        return x
+    end
+    return x*(math.random()+0.5)
+end
+
+local start = Game.start_run
+
+function Game:start_run(args)
+    start(self, args)
+
+    for name,center in pairs(G.P_CENTERS) do
+
+        if center.config and center.config.extra then
+            if type(center.config.extra) == "number" or (type(center.config.extra) == "table" and center.config.extra.tetrate) then
+
+                center.config = mod_number(center.config)
+
+            elseif type(center.config.extra) == "table" and (not center.config.extra.tetrate) then
+
+                for i,entry in pairs(center.config.extra) do
+                    if type(entry) == "number" or (type(entry) == "table" and entry.tetrate) then
+                        G.P_CENTERS[name].config.extra[i] = mod_number(entry)
+                    end
+                end
+
+            end
+        end
+
+    end
 end
